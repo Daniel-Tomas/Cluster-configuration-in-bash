4f7b0716a394728835e38c90daab0617b2e9cd74
Alejandro Cuadron Lafuente <33360440+Alexmontoril23@users.noreply.github.com>
2021-12-11 21:17:46 +0100

    Update lvm.sh

--- lvm.sh
+++ lvm.sh
@@ -8,22 +8,15 @@ then
 	exit 31
 fi
 
-cp $svc_cf conf
-ssh-keyscan -H ${host} 2>/dev/null >> ~/.ssh/known_hosts 
-scp conf ${host}:/root &>/dev/null
-
 ssh -T $host  >/dev/null << 'EOSSH' 
 
 perror() { echo -e "$@" 1>&2; }
-
 apt install -y lvm2 &>/dev/null
-
 if [[ $? -ne 0 ]]  
 then
   perror "Error en la instalacion de los paquetes necesarios"
   exit 32
 fi
-
 svc_cf="conf"
 i=0
 while ((i++)); IFS= read -r line
@@ -39,36 +32,35 @@ do
             if [[ ! -b $device ]]
 	        then
 	  	        perror "El siguiente dispositivo no existe o no es un dispositivo de bloque: \n$device"
-			    exit 35
+			    exit 33
 		    fi
             pvcreate $device
             if [[ $? -ne 0 ]]
             then
                 perror "Error en la iniciación del volumen físico"
-                exit 36
+                exit 34
             fi
         done
         vgcreate $vgn $line
         if [[ $? -ne 0 ]]
         then
             perror "Error en la creación del grupo de volúmenes"
-            exit 37
+            exit 35
         fi
     else
         read -ra lvp <<< "$line"
         if [[ ${#lvp[@]} -ne 2 ]]
         then
             perror "El número de elementos en esta línea es incorrecto. Se esperaban 2"
-            exit 38
+            exit 36
         fi
         lvcreate -L ${lvp[1]} -n ${lvp[0]} $vgn
         if [[ $? -ne 0 ]]
         then
             perror "Error en la creación del volumen lógico"
-            exit 39
+            exit 37
         fi
     fi
-
 done < "$svc_cf"
 
 EOSSH
